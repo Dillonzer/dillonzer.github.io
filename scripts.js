45ef731a15c9d8c6eb42c5c3754b4f100c0aed19
Dillonzer <dillonbast@gmail.com>
2022-02-10 10:54:20 -0600

    Updating PTCGBot

---
+++ scripts.js
@@ -0,0 +1,49 @@
+function Set(name, code, ptcgo_code, releaseDate)
+{
+    this.Name = name;
+    this.Code = code;
+    this.PTCGO_Code = ptcgo_code;
+    this.ReleaseDate = releaseDate;
+}
+
+
+let apiUrl = "https://ptcg-api.herokuapp.com"
+let allSets = [];
+
+function loadSetTable()
+{
+    GetAllSets()
+}
+
+function GetAllSets()
+{
+    var apiCall = apiUrl+"/api/sets";
+            fetch(apiCall).then(response => {
+            return response.json();
+            }).then(data => {
+                for(index in data) {
+                    allSets.push(new Set(data[index].name, data[index].code, data[index].ptcgoCode, data[index].releaseDate));
+                }
+
+                let setViewTable = document.getElementById("ptcgBotSets");
+                setViewTable.innerHTML = ""
+                for(let i = 0; i < allSets.length; i++)
+                {
+                    var rowCount = setViewTable.rows.length;
+                    var newRow = setViewTable.insertRow(rowCount);
+                    var cell1 = newRow.insertCell(0);
+                    var cell2 = newRow.insertCell(1);
+                    cell1.innerHTML = allSets[i].Name
+                    cell2.innerHTML = allSets[i].PTCGO_Code
+                }
+                var header = setViewTable.createTHead()                
+                var row = header.insertRow(0); 
+                var head1 = row.insertCell(0);
+                var head2 = row.insertCell(1);
+                head1.innerHTML = "Set Name"
+                head2.innerHTML = "Set Abbreviation"
+                
+            }).catch(err => {
+                console.log(err)
+            });
+}
